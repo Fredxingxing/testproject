d8bf3daf236cfb5340f3dd02a712cc666a4a94fe
Fredxingxing <33107804+Fredxingxing@users.noreply.github.com>
2019-05-12 18:33:58 +0800

    底部、联系我们

--- src/components/footerBar.vue
+++ src/components/footerBar.vue
@@ -1,13 +1,28 @@
 <template>
     <div class="footerBar">
-        <div></div>
-        <div>
-            <!--<router-link v-for="route in routerList" :to="route.path" :class="route.class" tag="div">-->
-                <!--{{route.name}}-->
-            <!--</router-link>-->
+        <div class="item">
+            <div class="title">福建熹龙律师事务所</div>
+            <p style="padding:0 30px;text-align: left">  所执业律师均具有学士以上学位，多名律师还拥有博士和硕士学位，受过系统专业的法学教育，
+                其中五年以上的执业经历者十几名，是一个有战斗力和凝聚力的律师团队</p>
         </div>
-        <div></div>
-        <div></div>
+        <div  class="item">
+            <div class="title">导航</div>
+            <router-link v-for="route in routerList" :to="route.path" :class="route.class" tag="div" style="cursor: pointer">
+                {{route.name}}
+            </router-link>
+        </div>
+        <div  class="item">
+            <div class="title">联系我们</div>
+            <div>福州市鼓楼区华林路100号茶叶大楼四层</div>
+            <div>0591-88568866</div>
+            <div>闽ICP备12009534号</div>
+        </div>
+        <div class="item">
+            <div class="title">营业时间</div>
+            <div>星期一 — 星期四 9:00 -21:00</div>
+            <div>星期五 8:00 - 21:00</div>
+            <div>星期六 9:30 - 15:00 </div>
+        </div>
     </div>
 </template>
 
@@ -51,13 +66,26 @@
 <style lang="less" scoped>
 .footerBar{
     width: 100%;
-    height:150px;
+    height:180px;
     z-index: 999;
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     align-items: center;
     background: #fff;
     box-shadow: 0px 1px 1px #aaa;
     border-top: 1px solid #d5d5d5;
+    .item{
+        flex:1;
+        height: 100%;
+        padding-top: 10px;
+        display: flex;
+        flex-direction: column;
+        font-size: 14px;
+        .title{
+            padding: 10px;
+            font-size: 18px;
+            color: #000;
+    }
+    }
 }
 </style>
--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        联系我们
+    <div class="about">
+        <h1 style="padding-top:100px;">联系我们</h1>
+        <div class="methods">
+            <div>电话：0591-88568866，传真：0591-87832711，邮编：350025</div>
+            <div>地址：福州市鼓楼区湖东路11号信息科技大楼3层（背靠省建设厅）</div>
+            <div>邮箱：liuguotian688@163.com。微信：lgt688。</div>
+            <div>新浪微博：在新浪微博中搜索“熹龙律师事务所”并加关注</div>
+            <div>战略联盟所：福建凌龙律师事务所 地址：莆田市荔城区艾力艾国际中心</div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +18,18 @@
 </script>
 
 <style scoped>
-
+.about{
+    height: 800px;
+}
+.methods{
+    margin-top: 100px;
+    padding: 0 30%;
+    height:200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    text-align: left;
+    font-size: 18px;
+}
 </style>
